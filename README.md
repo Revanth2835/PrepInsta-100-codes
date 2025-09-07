75ce1a577fd714b1304830096a82b69b966acd9c
Revanth2835 <revanthkumar2835@gmail.com>
2025-09-07 23:17:24 +0530

    first commit

--- README.md
+++ README.md
@@ -19,3 +19,4 @@
 "# primeFactors" 
 "# strongNumber" 
 "# perfectSquare" 
+"# automorphic" 
---
+++ automorphic.py
@@ -0,0 +1,8 @@
+n=55
+sq = n**2 
+strSq= str(sq) 
+
+if(strSq.endswith(str(n))) :
+    print(n,"is an automorphic number") 
+else:
+    print(n,"is not an automorphic number")
